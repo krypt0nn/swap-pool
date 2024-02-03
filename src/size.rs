e7cfc4d9b7c7e2c132335e2c536254a2f901d188
Observer KRypt0n_ <suimin.tu.mu.ga.mi@gmail.com>
2024-02-04 01:09:03 +0200

    feat: implement `SizeOf` for more types

--- src/size.rs
+++ src/size.rs
@@ -20,6 +20,14 @@ impl<T> SizeOf for T where T: size_of::SizeOf {
 }
 
 #[cfg(all(not(feature = "size-of-crate"), not(feature = "dyn-size-of-crate")))]
+impl<T, const N: usize> SizeOf for [T; N] where T: SizeOf {
+    #[inline]
+    fn size_of(&self) -> usize {
+        self.iter().map(T::size_of).sum::<usize>()
+    }
+}
+
+#[cfg(all(not(feature = "size-of-crate"), not(feature = "dyn-size-of-crate")))]
 impl<T> SizeOf for &[T] where T: SizeOf {
     #[inline]
     fn size_of(&self) -> usize {
@@ -76,19 +84,44 @@ macro_rules! impl_for_type {
             }
         }
     };
+
+    (deref $t:ty) => {
+        #[cfg(all(not(feature = "size-of-crate"), not(feature = "dyn-size-of-crate")))]
+        impl<T> SizeOf for $t where T: SizeOf {
+            #[inline]
+            fn size_of(&self) -> usize {
+                use std::ops::Deref;
+
+                std::mem::size_of_val(self) + self.deref().size_of()
+            }
+        }
+    };
+
+    ($t:ident$(,$f:ident)+) => {
+        impl_for_type!($t);
+        $(impl_for_type!($f);)+
+    }
 }
 
-impl_for_type!(i8);
-impl_for_type!(i16);
-impl_for_type!(i32);
-impl_for_type!(i64);
-impl_for_type!(i128);
+impl_for_type!(i8, i16, i32, i64, i128, isize);
+impl_for_type!(u8, u16, u32, u64, u128, usize);
+impl_for_type!(f32, f64);
+impl_for_type!(bool, char);
+impl_for_type!(());
 
-impl_for_type!(u8);
-impl_for_type!(u16);
-impl_for_type!(u32);
-impl_for_type!(u64);
-impl_for_type!(u128);
+impl_for_type!(std::sync::atomic::AtomicI8);
+impl_for_type!(std::sync::atomic::AtomicI16);
+impl_for_type!(std::sync::atomic::AtomicI32);
+impl_for_type!(std::sync::atomic::AtomicI64);
+impl_for_type!(std::sync::atomic::AtomicIsize);
+
+impl_for_type!(std::sync::atomic::AtomicU8);
+impl_for_type!(std::sync::atomic::AtomicU16);
+impl_for_type!(std::sync::atomic::AtomicU32);
+impl_for_type!(std::sync::atomic::AtomicU64);
+impl_for_type!(std::sync::atomic::AtomicUsize);
+
+impl_for_type!(std::sync::atomic::AtomicBool);
 
 impl_for_type!(len &str);
 impl_for_type!(len &std::ffi::OsStr);
@@ -96,3 +129,7 @@ impl_for_type!(len &std::ffi::OsStr);
 impl_for_type!(capacity String);
 impl_for_type!(capacity std::ffi::OsString);
 impl_for_type!(capacity std::path::PathBuf);
+
+impl_for_type!(deref Box<T>);
+impl_for_type!(deref std::rc::Rc<T>);
+impl_for_type!(deref std::sync::Arc<T>);
